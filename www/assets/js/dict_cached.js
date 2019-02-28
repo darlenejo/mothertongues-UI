6163bad2c9504a5160f0a53322179c580de49649
Darlene Joseph <47947903+darlenejo@users.noreply.github.com>
2019-02-28 13:52:35 -0800

    update

--- www/assets/js/dict_cached.js
+++ www/assets/js/dict_cached.js
@@ -1,4 +1,1 @@
-var dataDict = [
-    { "source": "CustomizeYourSource1", "definition": "sample definition 1", "optional": [{ "Sample Optional Information": "Unicode works: ❤ ☀ ☆ ☂ ☻ ♞ ☯ ☭ ☢ €" }], "word": "sample word 1", "sorting_form": [18], "entryID": "1", "audio": [{"filename": "https://www2.cs.uic.edu/~i101/SoundFiles/StarWars60.wav"}] },
-    { "source": "CustomizeYourSource2", "definition": "sample definition 2", "optional": [{ "Sample Optional Information": "Unicode works: ❤ ☀ ☆ ☂ ☻ ♞ ☯ ☭ ☢ €" }], "word": "sample word 2", "sorting_form": [18], "entryID": "2", "img": "https://via.placeholder.com/225?text=Sample+MTD+Image" }
-]
+var dataDict = [{"source": "Words", "definition": "to be here, to be close to, towards, speaker", "theme": "verb", "word": "\u0294\u00e9\u026be\u0294", "sorting_form": [30], "entryID": "1"}, {"source": "Words", "definition": "button", "theme": "noun", "word": "k\u02b7\u0259n\u00eds\u0259n", "sorting_form": [36, 39, 44, 39], "entryID": "0"}]
